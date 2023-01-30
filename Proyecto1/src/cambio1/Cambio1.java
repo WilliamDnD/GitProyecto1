e0e77df8d212c3d8aadecb81e6c2d894cdad651c
Guillermo O <gorivealonso@gmail.com>
2023-01-30 10:47:07 +0100

    Cambio1

---
+++ Proyecto1/src/cambio1/Cambio1.java
@@ -0,0 +1,13 @@
+package cambio1;
+
+public class Cambio1 {
+
+	public static void main(String[] args) {
+		System.out.println("Cambio1");
+		System.out.println("stdh");
+		System.out.println(" rdfs");
+		System.out.println("Test");
+
+	}
+
+}
